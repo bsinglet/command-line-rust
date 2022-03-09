ed5d81bb30c5aa8c3599a4737d1c2a51b26de4dd
Benjamin M. Singleton <bsinglet@vt.edu>
2022-03-09 00:27:15 -0500

    Implemented the --squeeze-blanks, and --show-ends options.

--- chapter_03/src/lib.rs
+++ chapter_03/src/lib.rs
@@ -8,6 +8,8 @@ pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+    show_ends: bool,
+    squeeze_blank: bool,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -18,19 +20,30 @@ pub fn run(config: Config) -> MyResult<()> {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
             Ok(buf) => {
                 let mut last_num = 0;
+                let mut previous_line_blank = false;
+                let line_ending = if config.show_ends {"$"} else {""};
                 for (line_number, each_line) in buf.lines().enumerate() {
                     let each_line = each_line?;
+                    // skip consecutive blank lines with -s
+                    if config.squeeze_blank && each_line.is_empty() && previous_line_blank {
+                                continue;
+                    }
+                    if each_line.is_empty() {
+                        previous_line_blank = true;
+                    }else {
+                        previous_line_blank = false;
+                    }
                     if config.number_lines {
-                        println!("{:>6}\t{}", line_number + 1, each_line);
+                        println!("{:>6}\t{}{}", line_number + 1, each_line, line_ending);
                     }else if config.number_nonblank_lines {
                         if !each_line.is_empty() {
                             last_num += 1;
-                            println!("{:>6}\t{}", last_num, each_line);
+                            println!("{:>6}\t{}{}", last_num, each_line, line_ending);
                         }else {
-                            println!();
+                            println!("{}", line_ending);
                         }
                     }else {
-                        println!("{}", each_line);
+                        println!("{}{}", each_line, line_ending);
                     }
                 }
             },
@@ -73,11 +86,27 @@ pub fn get_args() -> MyResult<Config> {
                 .help("Number non-blank lines")
                 .takes_value(false),
         )
+        .arg(
+            Arg::with_name("show_ends")
+                .short("E")
+                .long("show-ends")
+                .help("Display $ at end of each line")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("squeeze_blank")
+                .short("s")
+                .long("squeeze-blank")
+                .help("Suppress repeated empty output lines")
+                .takes_value(false)
+        )
         .get_matches();
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
         number_lines: matches.is_present("number"),
         number_nonblank_lines: matches.is_present("number_nonblank"),
+        show_ends: matches.is_present("show_ends"),
+        squeeze_blank: matches.is_present("squeeze_blank"),
     })
 }
