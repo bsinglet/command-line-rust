45719fc34998376d8865b1d587c5e6373f4c1cd6
Benjamin M. Singleton <bsinglet@vt.edu>
2022-03-28 01:21:05 -0400

    Version from the book passing all tests.

--- chapter_04/src/lib.rs
+++ chapter_04/src/lib.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -79,24 +79,39 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
 pub fn run(config: Config) -> MyResult<()> {
     let num_files = config.files.len();
 
-    for filename in config.files {
+    for (file_num, filename) in config.files.iter().enumerate() {
         match open(&filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(buf) => {
+            Ok(mut file) => {
                 if num_files > 1 {
-                    println!("==> {} <==", filename);
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        filename
+                    );
                 }
-                match config.bytes {
-                    Some(bytes) => println!("You specified how many bytes to read?"),
-                    None => {
-                        for each_line in buf.lines().take(config.lines) {
-                            let each_line = each_line?;
-                            print!("{}", each_line);
+
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!(
+                        "{}",
+                        String::from_utf8_lossy(&buffer[..bytes_read])
+                    );
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
                         }
-                    },
+                        print!("{}", line);
+                        line.clear();
+                    }
                 }
-            },
-        }
+            }
+        };
     }
     Ok(())
 }
