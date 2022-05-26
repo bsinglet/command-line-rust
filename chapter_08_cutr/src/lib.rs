bcd780bf1ed438bbd93f68bbdfc5faf4c8532ea9
Benjamin M. Singleton <bsinglet@vt.edu>
2022-05-25 22:11:29 -0400

    Initial commit, skeletons copied from beginning of the chapter.

---
+++ chapter_08_cutr/src/lib.rs
@@ -0,0 +1,44 @@
+use crate::Extract::*;
+use clap::{App, Arg};
+use std::{error::Error, ops::Range};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("cutr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust cut")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        files: ...
+        delimiter: ...
+        extract: ...
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", &config);
+    Ok(())
+}
+
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+    unimplemented!();
+}
